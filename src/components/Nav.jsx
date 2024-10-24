1798cd1077637c161ffa46e1f9fa342339e0123c
João Viktor <viktorcarvalhos@gmail.com>
2024-10-23 22:00:59 -0300

    fix: nav logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ function Nav() {
     return (
         <header className="nav-header">
             <div className="nav-logo">
-                <img src="..\..\static\images\logo.png" alt="Logo" className="logo-image" />
+                <img src="..\..\public\logo.png" alt="Logo" className="logo-image" />
             </div>
         </header>
     );
