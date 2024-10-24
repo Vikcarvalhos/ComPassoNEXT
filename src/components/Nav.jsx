55ca6a6f146c1477abd6681e9f211ea9a500e1d1
João Viktor <viktorcarvalhos@gmail.com>
2024-10-23 22:06:32 -0300

    fix: nav logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Logo } from '../../public/logo.png';
+import Logo from '../logo.png';
 import '../css/nav.css';
 
 function Nav() {
     return (
         <header className="nav-header">
             <div className="nav-logo">
-                <img src="..\..\public\logo.png" alt="Logo" className="logo-image" />
+                <img src={Logo} alt="Logo" className="logo-image" />
             </div>
         </header>
     );
